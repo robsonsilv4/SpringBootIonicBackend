8309a7786edf1b576d0bda694ea8488d1fc7c49a
robsonsilv4 <robsonsilv410@gmail.com>
2018-05-26 20:33:50 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/robson/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.robson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robson.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
