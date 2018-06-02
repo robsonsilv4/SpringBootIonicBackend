bc702d0232ed9df3da88df2926c9820ce7f8aaa3
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-01 21:31:16 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/robson/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.robson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robson.cursomc.domain.Cliente;
+import com.robson.cursomc.repositories.ClienteRepository;
+import com.robson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
