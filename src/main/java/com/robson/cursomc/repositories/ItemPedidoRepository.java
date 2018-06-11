df5c5cec24b506df148857f8481bd013c1eafc5a
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-11 08:23:16 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/robson/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.robson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robson.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
