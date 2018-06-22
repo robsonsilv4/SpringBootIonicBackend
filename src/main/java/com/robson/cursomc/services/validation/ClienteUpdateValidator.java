b0d0ef22555ee0eacd821d59bdede3e2d12d3a94
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-22 14:55:21 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/robson/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/robson/cursomc/services/validation/ClienteUpdateValidator.java
@@ -2,41 +2,42 @@ package com.robson.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.robson.cursomc.domain.Cliente;
-import com.robson.cursomc.domain.enums.TipoCliente;
-import com.robson.cursomc.dto.ClienteNewDTO;
+import com.robson.cursomc.dto.ClienteDTO;
 import com.robson.cursomc.repositories.ClienteRepository;
 import com.robson.cursomc.resources.exception.FieldMessage;
-import com.robson.cursomc.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	@Autowired
+	private HttpServletRequest request;
+
 	@Autowired
 	ClienteRepository repo;
 
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request
+				.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+
 		List<FieldMessage> list = new ArrayList<>();
 
-		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-
-		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
-
 		Cliente aux = repo.findByEmail(objDto.getEmail());
-		if (aux.getEmail() != null) {
+		if (aux.getEmail() != null && !aux.getId().equals(uriId)) {
 			list.add(new FieldMessage("Email", "Email já existente"));
 		}
 
