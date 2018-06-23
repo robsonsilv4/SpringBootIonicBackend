23f899d7bc7f7745ac6198341041035afab31dc4
robsonsilv4 <robsonsilv410@gmail.com>
2018-06-23 11:05:14 -0300

    Implementando toString de Pedido

--- src/main/java/com/robson/cursomc/domain/ItemPedido.java
+++ src/main/java/com/robson/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.robson.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -68,11 +70,11 @@ public class ItemPedido implements Serializable {
 		this.desconto = desconto;
 	}
 
-	public Integer getQuatidade() {
+	public Integer getQuantidade() {
 		return quantidade;
 	}
 
-	public void setQuatidade(Integer quatidade) {
+	public void setQuantidade(Integer quatidade) {
 		this.quantidade = quatidade;
 	}
 
@@ -107,5 +109,20 @@ public class ItemPedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qte: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
 	}
 }
--- src/main/java/com/robson/cursomc/services/PedidoService.java
+++ src/main/java/com/robson/cursomc/services/PedidoService.java
@@ -27,6 +27,9 @@ public class PedidoService {
 
 	@Autowired
 	ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -37,6 +40,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstance(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setPagamento(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -46,10 +50,12 @@ public class PedidoService {
 		obj = repo.save(obj);
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
