780f2d770da8f6e780fb6cabfa6ce15a0d85f20d
robsonsilv4 <robsonsilv410@gmail.com>
2018-07-02 21:55:15 -0300

    Restrição de conteúdo: Cliente só recupera ele mesmo

--- src/main/java/com/robson/cursomc/services/ClienteService.java
+++ src/main/java/com/robson/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.robson.cursomc.domain.Cidade;
 import com.robson.cursomc.domain.Cliente;
 import com.robson.cursomc.domain.Endereco;
+import com.robson.cursomc.domain.enums.Perfil;
 import com.robson.cursomc.domain.enums.TipoCliente;
 import com.robson.cursomc.dto.ClienteDTO;
 import com.robson.cursomc.dto.ClienteNewDTO;
 import com.robson.cursomc.repositories.ClienteRepository;
 import com.robson.cursomc.repositories.EnderecoRepository;
+import com.robson.cursomc.security.UserSS;
+import com.robson.cursomc.services.exceptions.AuthorizationException;
 import com.robson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,6 +37,11 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
---
+++ src/main/java/com/robson/cursomc/services/UserService.java
@@ -0,0 +1,15 @@
+package com.robson.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.robson.cursomc.security.UserSS;
+
+public class UserService {
+	public static UserSS authenticated() {
+		try {
+		return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
